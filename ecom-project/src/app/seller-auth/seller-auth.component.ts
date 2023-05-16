41c9c8b2b5618d86d80faff0c1ad766302b17b39
Azad Bagirov <azad.bagirov@Azads-MacBook-Pro.local>
2023-05-16 21:58:35 +0400

    seller-home injected to auth-component

--- ecom-project/src/app/seller-auth/seller-auth.component.ts
+++ ecom-project/src/app/seller-auth/seller-auth.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { SellerService } from '../services/seller.service';
-
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.css']
 })
 export class SellerAuthComponent implements OnInit{
-  constructor(private sellerService:SellerService){}
+  constructor(private sellerService:SellerService, private router: Router){}
   
   ngOnInit(): void {}
 
   signUp(data:object):void{
     this.sellerService.userSignUp(data).subscribe((result)=>{
-      console.warn(result)
+      if(result){
+        this.router.navigate(['seller-home'])
+      }
     });
   }
 
