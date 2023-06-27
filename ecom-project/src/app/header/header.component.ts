bd12decdb27b7a995026ddaddd4c35e375249fc9
cavadjava <cavadjava@gmail.com>
2023-06-27 21:58:47 +0400

    update Navbar menu for seller after login

--- ecom-project/src/app/header/header.component.ts
+++ ecom-project/src/app/header/header.component.ts
@@ -7,17 +7,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit{
-
+  menuType: string = 'default';
   constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.router.events.subscribe((val:any) => {
       if (val.url) {
-        console.warn(val.url)
         if (localStorage.getItem('seller') && val.url.includes('seller')) {
           console.log('in seller area')
+          this.menuType = 'seller'
         } else {
           console.log('outside seller')
+          this.menuType = 'default'
         }
       }
     })
