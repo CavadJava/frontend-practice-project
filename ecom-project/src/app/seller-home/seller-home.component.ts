d407fbb70025a9df3a31d066e7dd0d826b223ca9
cavadjava <cavadjava@gmail.com>
2023-07-02 01:47:35 +0400

    get api result in component

--- ecom-project/src/app/seller-home/seller-home.component.ts
+++ ecom-project/src/app/seller-home/seller-home.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../model/Product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
   styleUrls: ['./seller-home.component.css']
 })
-export class SellerHomeComponent { }
+export class SellerHomeComponent implements OnInit{
+
+  productList: Product[] | undefined;
+ 
+  constructor(private productService: ProductService) {
+    this.productService = productService;
+  }
+
+  ngOnInit() : void {
+    this.productService.getProductList().subscribe((result)=>{
+      if(result) {
+        this.productList = result
+        console.warn(result);
+      }
+    })
+  }
+}
