5dccf48bd5bc591eb743472f2680b34c30f3976a
cavadjava <cavadjava@gmail.com>
2023-07-02 01:47:03 +0400

    make service function to call api

--- ecom-project/src/app/services/product.service.ts
+++ ecom-project/src/app/services/product.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Product } from '../model/Product';
 
 @Injectable({
@@ -11,5 +12,8 @@ export class ProductService {
 
   addProduct(data: Product) {
     return this.httpClient.post("http://localhost:3000/products",data)
+  }
+  getProductList() :Observable<Product[]> {
+    return this.httpClient.get<Product[]>("http://localhost:3000/products");
   }
 }
