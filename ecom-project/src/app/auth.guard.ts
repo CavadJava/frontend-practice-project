3569a9226ce55c0e1d6f89f20bc0d7ec0c6016c8
javad <javad.bagirov@unibank.az>
2023-07-05 18:50:37 +0400

    guard optization

--- ecom-project/src/app/auth.guard.ts
+++ ecom-project/src/app/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Route, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { map, Observable, take, tap } from 'rxjs';
+import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Route, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { BehaviorSubject, map, Observable, take, tap } from 'rxjs';
 import { SellerService } from './services/seller.service';
 
 // export const authGuard: CanActivateFn = (route, state) => {
@@ -13,6 +13,7 @@ import { SellerService } from './services/seller.service';
 
 export class authGuard implements CanActivate {
   constructor(
+    private router: Router,
     private sellerService: SellerService
   ) { 
     console.warn("guard-step1")
@@ -20,7 +21,14 @@ export class authGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> {
-      console.warn("guard-step2"+this.sellerService.isSellerLoggedIn.value);
-    return this.sellerService.isSellerLoggedIn;
+      // console.warn("guard-step2"+this.sellerService.isSellerLoggedIn.value);
+    // return this.sellerService.isSellerLoggedIn;
+    let logged = this.sellerService.isSellerLoggedIn;
+    if(logged) {
+      return logged;
+    }
+    console.log("seller-auth by auth")
+    this.router.navigate(["seller-home"]);
+    return new BehaviorSubject<boolean>(true);
   }
 }
