e90f4e20a1c99ebacb997222a3f4b7b03e6ead86
Javad Bagirov <javad.bagirov@unibank.az>
2023-08-21 13:11:28 +0400

    adde ejs libs and view engine

--- nodejs-series-practice/node-crash-course/app.js
+++ nodejs-series-practice/node-crash-course/app.js
@@ -2,17 +2,22 @@ const express = require('express')
 
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+// app.set("views", "myviews");
 
 // listen for requests
 app.listen(3000)
 
 app.get("/", (req,resp)=>{
-    resp.sendFile('./views/index.html',{root:__dirname});
+    // resp.sendFile('./old-views/index.html',{root:__dirname});
+    resp.render("index")
 });
 
 app.get("/about", (req,resp)=>{
 // resp.send("<p>Hello javad, welcome to mean stack</p>");
-resp.sendFile('./views/about.html',{root:__dirname});
+// resp.sendFile('./old-views/about.html',{root:__dirname});
+    resp.render("about")
 });
 
 
@@ -23,5 +28,6 @@ app.get("/about-me", (req,resp)=>{
 
 // 404 page
 app.use((req, resp)=>{
-    resp.sendFile("./views/404.html",{root:__dirname})
+    resp.render("404");
+    // resp.sendFile("./old-views/404.html",{root:__dirname})
 });
